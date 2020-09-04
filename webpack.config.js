f49da0467e5da3e58ac238603239c908e786c4b1
Dan Rudd <thisisruddy@gmail.com>
2020-09-04 16:40:58 +0100

    Revert "Various updates"
    This reverts commit 4153f1425cac17bedb6de77d477761c8e62306ae.

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,15 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const Nodemon = require('nodemon-webpack-plugin');
 
-module.exports = (env, argv) => ({
-    mode: argv.mode || 'development',
+module.exports = {
     target: 'node',
-    entry: './src/index.js',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src')
         }
     },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
     plugins: [
-        new Dotenv({
-            path: `.env.${argv.mode}`
-        }),
+        new Dotenv(),
         new Nodemon()
     ]
-})
+}
