4153f1425cac17bedb6de77d477761c8e62306ae
Dan Rudd <thisisruddy@gmail.com>
2020-09-04 16:37:30 +0100

    Various updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,30 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const Nodemon = require('nodemon-webpack-plugin');
 
-module.exports = {
+module.exports = (env, argv) => ({
+    mode: argv.mode || 'development',
     target: 'node',
+    entry: './src/index.js',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src')
         }
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
     plugins: [
-        new Dotenv(),
+        new Dotenv({
+            path: `.env.${argv.mode}`
+        }),
         new Nodemon()
     ]
-}
+})
